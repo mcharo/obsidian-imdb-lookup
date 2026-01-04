97d7f548b2edc37c5a2f99861a8871eb6f5caca8
Michael Charo <michael@opstack.com>
2026-01-04 08:57:52 -0600

    Initial version

--- README.md
+++ README.md
@@ -1,90 +1,99 @@
-# Obsidian Sample Plugin
+# IMDB Lookup Plugin for Obsidian
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+Sync movie data from the OMDB API directly into your Obsidian notes.
 
-This project uses TypeScript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in TypeScript Definition format, which contains TSDoc comments describing what it does.
+## Features
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open modal (simple)" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+- Automatically fetch movie metadata using IMDB IDs
+- Configurable target folder for movie notes
+- Customizable field mappings (choose which OMDB fields map to which note properties)
+- Sync individual notes or batch sync entire folder
+- Rate-limited API calls to respect OMDB limits
 
-## First time developing plugins?
+## Setup
 
-Quick starting guide for new plugin devs:
+1. Get a free API key from [OMDB API](https://www.omdbapi.com/apikey.aspx)
+2. Install the plugin
+3. Go to Settings → IMDB Lookup
+4. Enter your OMDB API key
+5. Configure your target folder (default: "Movies")
+6. Customize field mappings as needed
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+## Usage
 
-## Releasing new releases
+### Note Format
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+Your movie notes need an IMDB ID in the frontmatter:
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
-
-## Adding your plugin to the community plugin list
-
-- Check the [plugin guidelines](https://docs.obsidian.md/Plugins/Releasing/Plugin+guidelines).
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-## How to use
-
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
-
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- This project already has eslint preconfigured, you can invoke a check by running`npm run lint`
-- Together with a custom eslint [plugin](https://github.com/obsidianmd/eslint-plugin) for Obsidan specific code guidelines.
-- A GitHub action is preconfigured to automatically lint every commit on all branches.
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
+```yaml
+---
+imdbid: tt3896198
+---
+# Guardians of the Galaxy Vol. 2
 ```
 
-If you have multiple URLs, you can also do:
+The IMDB ID can be found in any IMDB URL, e.g., `https://www.imdb.com/title/tt3896198/`
 
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
+### Commands
+
+- **Sync all movies from OMDB**: Syncs all notes in your target folder that have an IMDB ID
+- **Sync current note from OMDB**: Syncs only the currently open note
+
+### After Syncing
+
+Your note frontmatter will be updated with the configured fields:
+
+```yaml
+---
+imdbid: tt3896198
+title: Guardians of the Galaxy Vol. 2
+year: "2017"
+rated: PG-13
+released: 05 May 2017
+runtime: 136 min
+genre: Action, Adventure, Comedy
+director: James Gunn
+actors: Chris Pratt, Zoe Saldaña, Dave Bautista
+plot: The Guardians struggle to keep together as a team...
+poster: https://m.media-amazon.com/images/M/...
+imdbrating: "7.6"
+---
 ```
 
-## API Documentation
+## Settings
 
-See https://docs.obsidian.md
+### API Key
+Your OMDB API key (required).
+
+### Target Folder
+The folder containing your movie notes. Default: `Movies`
+
+### IMDB ID Property
+The frontmatter property name containing the IMDB ID. Default: `imdbid`
+
+### Field Mappings
+Configure which OMDB fields to sync and what property names to use in your notes. Toggle fields on/off and customize the property names.
+
+Available OMDB fields:
+- Title, Year, Rated, Released, Runtime
+- Genre, Director, Writer, Actors
+- Plot, Language, Country, Awards
+- Poster, Metascore, imdbRating, imdbVotes
+- Type, DVD, BoxOffice, Production, Website
+
+## Development
+
+```bash
+# Install dependencies
+npm install
+
+# Build for development (watch mode)
+npm run dev
+
+# Build for production
+npm run build
+```
+
+## License
+
+BSD
--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,365 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
-
-// Remember to rename these classes and interfaces!
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+import {
+	App,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+	TFolder,
+	requestUrl,
+  } from "obsidian";
+  
+  interface FieldMapping {
+	omdbField: string;
+	noteProperty: string;
+	enabled: boolean;
+  }
+  
+  interface IMDBLookupSettings {
+	apiKey: string;
+	targetFolder: string;
+	imdbIdProperty: string;
+	fieldMappings: FieldMapping[];
+  }
+  
+  const OMDB_FIELDS = [
+	"Title",
+	"Year",
+	"Rated",
+	"Released",
+	"Runtime",
+	"Genre",
+	"Director",
+	"Writer",
+	"Actors",
+	"Plot",
+	"Language",
+	"Country",
+	"Awards",
+	"Poster",
+	"Metascore",
+	"imdbRating",
+	"imdbVotes",
+	"Type",
+	"DVD",
+	"BoxOffice",
+	"Production",
+	"Website",
+  ];
+  
+  const DEFAULT_SETTINGS: IMDBLookupSettings = {
+	apiKey: "",
+	targetFolder: "Movies",
+	imdbIdProperty: "imdbid",
+	fieldMappings: [
+	  { omdbField: "Title", noteProperty: "title", enabled: true },
+	  { omdbField: "Year", noteProperty: "year", enabled: true },
+	  { omdbField: "Rated", noteProperty: "rated", enabled: true },
+	  { omdbField: "Released", noteProperty: "released", enabled: true },
+	  { omdbField: "Runtime", noteProperty: "runtime", enabled: true },
+	  { omdbField: "Genre", noteProperty: "genre", enabled: true },
+	  { omdbField: "Director", noteProperty: "director", enabled: true },
+	  { omdbField: "Writer", noteProperty: "writer", enabled: false },
+	  { omdbField: "Actors", noteProperty: "actors", enabled: true },
+	  { omdbField: "Plot", noteProperty: "plot", enabled: true },
+	  { omdbField: "Language", noteProperty: "language", enabled: false },
+	  { omdbField: "Country", noteProperty: "country", enabled: false },
+	  { omdbField: "Awards", noteProperty: "awards", enabled: false },
+	  { omdbField: "Poster", noteProperty: "poster", enabled: true },
+	  { omdbField: "Metascore", noteProperty: "metascore", enabled: false },
+	  { omdbField: "imdbRating", noteProperty: "imdbrating", enabled: true },
+	  { omdbField: "imdbVotes", noteProperty: "imdbvotes", enabled: false },
+	  { omdbField: "Type", noteProperty: "type", enabled: false },
+	  { omdbField: "DVD", noteProperty: "dvd", enabled: false },
+	  { omdbField: "BoxOffice", noteProperty: "boxoffice", enabled: false },
+	  { omdbField: "Production", noteProperty: "production", enabled: false },
+	  { omdbField: "Website", noteProperty: "website", enabled: false },
+	],
+  };
+  
+  interface OMDBResponse {
+	Response: string;
+	Error?: string;
+	[key: string]: string | undefined | OMDBRating[];
+	Ratings?: OMDBRating[];
+  }
+  
+  interface OMDBRating {
+	Source: string;
+	Value: string;
+  }
+  
+  export default class IMDBLookupPlugin extends Plugin {
+	settings: IMDBLookupSettings = DEFAULT_SETTINGS;
+  
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+	  await this.loadSettings();
+  
+	  this.addCommand({
+		id: "sync-all-movies",
+		name: "Sync all movies from OMDB",
+		callback: () => this.syncAllMovies(),
+	  });
+  
+	  this.addCommand({
+		id: "sync-current-note",
+		name: "Sync current note from OMDB",
+		callback: () => this.syncCurrentNote(),
+	  });
+  
+	  this.addSettingTab(new IMDBLookupSettingTab(this.app, this));
 	}
-
-	onunload() {
-	}
-
+  
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
+	  this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	  // Merge any new default field mappings that might have been added
+	  const existingFields = new Set(
+		this.settings.fieldMappings.map((m) => m.omdbField)
+	  );
+	  for (const defaultMapping of DEFAULT_SETTINGS.fieldMappings) {
+		if (!existingFields.has(defaultMapping.omdbField)) {
+		  this.settings.fieldMappings.push({ ...defaultMapping });
+		}
+	  }
 	}
-
+  
 	async saveSettings() {
-		await this.saveData(this.settings);
+	  await this.saveData(this.settings);
 	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+  
+	async syncCurrentNote() {
+	  const activeFile = this.app.workspace.getActiveFile();
+	  if (!activeFile) {
+		new Notice("No active file");
+		return;
+	  }
+  
+	  if (activeFile.extension !== "md") {
+		new Notice("Active file is not a markdown file");
+		return;
+	  }
+  
+	  await this.syncNote(activeFile);
 	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
+  
+	async syncAllMovies() {
+	  if (!this.settings.apiKey) {
+		new Notice("Please configure your OMDB API key in settings");
+		return;
+	  }
+  
+	  const folder = this.app.vault.getAbstractFileByPath(
+		this.settings.targetFolder
+	  );
+	  if (!folder || !(folder instanceof TFolder)) {
+		new Notice(`Folder "${this.settings.targetFolder}" not found`);
+		return;
+	  }
+  
+	  const files = this.getMarkdownFilesRecursively(folder);
+	  if (files.length === 0) {
+		new Notice(`No markdown files found in "${this.settings.targetFolder}"`);
+		return;
+	  }
+  
+	  new Notice(`Starting sync for ${files.length} files...`);
+  
+	  let synced = 0;
+	  let skipped = 0;
+	  let errors = 0;
+  
+	  for (const file of files) {
+		try {
+		  const result = await this.syncNote(file, true);
+		  if (result === "synced") synced++;
+		  else if (result === "skipped") skipped++;
+		  else errors++;
+		} catch (e) {
+		  console.error(`Error syncing ${file.path}:`, e);
+		  errors++;
+		}
+		// Rate limiting - OMDB free tier has limits
+		await this.sleep(250);
+	  }
+  
+	  new Notice(
+		`Sync complete: ${synced} synced, ${skipped} skipped, ${errors} errors`
+	  );
 	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+  
+	getMarkdownFilesRecursively(folder: TFolder): TFile[] {
+	  const files: TFile[] = [];
+	  for (const child of folder.children) {
+		if (child instanceof TFile && child.extension === "md") {
+		  files.push(child);
+		} else if (child instanceof TFolder) {
+		  files.push(...this.getMarkdownFilesRecursively(child));
+		}
+	  }
+	  return files;
 	}
-}
+  
+	async syncNote(
+	  file: TFile,
+	  silent = false
+	): Promise<"synced" | "skipped" | "error"> {
+	  const cache = this.app.metadataCache.getFileCache(file);
+	  const frontmatter = cache?.frontmatter;
+  
+	  if (!frontmatter) {
+		if (!silent) new Notice(`No frontmatter found in ${file.name}`);
+		return "skipped";
+	  }
+  
+	  const imdbId = frontmatter[this.settings.imdbIdProperty];
+	  if (!imdbId) {
+		if (!silent)
+		  new Notice(
+			`No ${this.settings.imdbIdProperty} property found in ${file.name}`
+		  );
+		return "skipped";
+	  }
+  
+	  if (!this.settings.apiKey) {
+		if (!silent) new Notice("Please configure your OMDB API key in settings");
+		return "error";
+	  }
+  
+	  try {
+		const data = await this.fetchOMDBData(imdbId);
+		if (!data || data.Response === "False") {
+		  if (!silent)
+			new Notice(`OMDB Error: ${data?.Error || "Unknown error"}`);
+		  return "error";
+		}
+  
+		await this.updateNoteFrontmatter(file, data);
+		if (!silent) new Notice(`Synced: ${file.name}`);
+		return "synced";
+	  } catch (e) {
+		console.error(`Error fetching OMDB data for ${imdbId}:`, e);
+		if (!silent) new Notice(`Error syncing ${file.name}`);
+		return "error";
+	  }
+	}
+  
+	async fetchOMDBData(imdbId: string): Promise<OMDBResponse | null> {
+	  const url = `https://www.omdbapi.com/?i=${encodeURIComponent(imdbId)}&apikey=${this.settings.apiKey}`;
+	  try {
+		const response = await requestUrl({ url });
+		return response.json as OMDBResponse;
+	  } catch (e) {
+		console.error("OMDB API request failed:", e);
+		return null;
+	  }
+	}
+  
+	async updateNoteFrontmatter(file: TFile, data: OMDBResponse) {
+	  await this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+		for (const mapping of this.settings.fieldMappings) {
+		  if (!mapping.enabled) continue;
+  
+		  const value = data[mapping.omdbField];
+		  if (value !== undefined && value !== "N/A") {
+			frontmatter[mapping.noteProperty] = value;
+		  }
+		}
+	  });
+	}
+  
+	sleep(ms: number): Promise<void> {
+	  return new Promise((resolve) => setTimeout(resolve, ms));
+	}
+  }
+  
+  class IMDBLookupSettingTab extends PluginSettingTab {
+	plugin: IMDBLookupPlugin;
+  
+	constructor(app: App, plugin: IMDBLookupPlugin) {
+	  super(app, plugin);
+	  this.plugin = plugin;
+	}
+  
+	display(): void {
+	  const { containerEl } = this;
+	  containerEl.empty();
+  
+	  containerEl.createEl("h2", { text: "IMDB Lookup Settings" });
+  
+	  new Setting(containerEl)
+		.setName("OMDB API Key")
+		.setDesc("Your OMDB API key (get one at omdbapi.com)")
+		.addText((text) =>
+		  text
+			.setPlaceholder("Enter your API key")
+			.setValue(this.plugin.settings.apiKey)
+			.onChange(async (value) => {
+			  this.plugin.settings.apiKey = value;
+			  await this.plugin.saveSettings();
+			})
+		);
+  
+	  new Setting(containerEl)
+		.setName("Target Folder")
+		.setDesc("The folder containing your movie notes")
+		.addText((text) =>
+		  text
+			.setPlaceholder("Movies")
+			.setValue(this.plugin.settings.targetFolder)
+			.onChange(async (value) => {
+			  this.plugin.settings.targetFolder = value;
+			  await this.plugin.saveSettings();
+			})
+		);
+  
+	  new Setting(containerEl)
+		.setName("IMDB ID Property")
+		.setDesc("The frontmatter property name containing the IMDB ID")
+		.addText((text) =>
+		  text
+			.setPlaceholder("imdbid")
+			.setValue(this.plugin.settings.imdbIdProperty)
+			.onChange(async (value) => {
+			  this.plugin.settings.imdbIdProperty = value;
+			  await this.plugin.saveSettings();
+			})
+		);
+  
+	  containerEl.createEl("h3", { text: "Field Mappings" });
+	  containerEl.createEl("p", {
+		text: "Configure which OMDB fields to sync and their property names in your notes.",
+		cls: "setting-item-description",
+	  });
+  
+	  const mappingsContainer = containerEl.createDiv({
+		cls: "imdb-lookup-mappings",
+	  });
+  
+	  for (const mapping of this.plugin.settings.fieldMappings) {
+		const mappingEl = mappingsContainer.createDiv({
+		  cls: "imdb-lookup-mapping",
+		});
+  
+		new Setting(mappingEl)
+		  .setName(mapping.omdbField)
+		  .setDesc(`Maps to property: ${mapping.noteProperty}`)
+		  .addToggle((toggle) =>
+			toggle.setValue(mapping.enabled).onChange(async (value) => {
+			  mapping.enabled = value;
+			  await this.plugin.saveSettings();
+			})
+		  )
+		  .addText((text) =>
+			text
+			  .setPlaceholder(mapping.omdbField.toLowerCase())
+			  .setValue(mapping.noteProperty)
+			  .onChange(async (value) => {
+				mapping.noteProperty = value || mapping.omdbField.toLowerCase();
+				await this.plugin.saveSettings();
+			  })
+		  );
+	  }
+	}
+  }
+  
+  
